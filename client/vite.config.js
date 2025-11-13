35834489c721a46726208e989038f77d10b759aa
Davi Dorea <ddorea65@gmail.com>
2025-11-13 17:23:30 -0300

    deploy: trigger build

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/quem-sou-eu-ao-vivo/",
   plugins: [react()],
-  base: "/quem-sou-eu-ao-vivo/",  // 👈 nome exato do repositório!
 });
